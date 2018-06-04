ce41d10ba461025a8f248afd69e25562c0d94adb
sai <saibabu.edepalli@gmail.com>
2018-06-04 13:10:36 +0530

    My Project setup Completed Successfully

---
+++ EHealthcareBackend/src/main/java/sai/pro/EHealthcareBackend/App.java
@@ -0,0 +1,13 @@
+package sai.pro.EHealthcareBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
